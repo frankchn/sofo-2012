3218a7327f501c11c12312ea415a6aa39fcc3280
Frank Chen <frankchn@gmail.com>
2012-03-27 01:39:10 -0700

    slight formatting and style changes

---
+++ circuits/home/templates/dashboard.php
@@ -0,0 +1,24 @@
+<div class="section_container">
+	<div class="section_actions">Upload New Photos</div>
+	<h2>My Photostream</h2>
+	<div class="section_content">
+	
+	</div>
+</div>
+
+<div class="section_container">
+	<div class="section_actions">Manage Sets</div>
+	<h2>My Sets</h2>
+	<div class="section_content">
+	
+	</div>
+</div>
+
+
+<div class="section_container">
+	<div class="section_actions">Add Friends</div>
+	<h2>My Friends</h2>
+	<div class="section_content">
+	
+	</div>
+</div>
--- circuits/home/welcome.php
+++ circuits/home/welcome.php
@@ -1,5 +1,9 @@
 <?php
 
-
-$template = new Template('templates/welcome.php');
-$template_manager->add_template('content', $template);
+if($user == null) {
+	$template = new Template('templates/welcome.php');
+	$template_manager->add_template('content', $template);
+} else {
+	$template = new Template('templates/dashboard.php');
+	$template_manager->add_template('content', $template);
+}
